03f7b3297d525740c96d6035c06d7be0a6fc2a8f
danuux <danut_laurentiu98@yahoo.com>
2024-08-30 20:07:35 +0300

    Added header and background

---
+++ ev-website/src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import './Hero.css'
+import arrow_btn from '../../assets/arrow_btn.png'
+import play_icon from '../../assets/play_icon.png'
+import pause_icon from '../../assets/pause_icon.png'
+const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+  return (
+    <div className='hero'>
+      
+    </div>
+  )
+}
+
+export default Hero
